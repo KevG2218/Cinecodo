8e328628eedc07d7b27ab9bc313e178f6acab9e3
KevG2218 <116507273+KevG2218@users.noreply.github.com>
2024-03-27 12:38:15 -0300

    Add files via upload

---
+++ practico codo a codo/script.js
@@ -0,0 +1,14 @@
+const toggleButtons = document.querySelectorAll('.toggle-button');
+
+toggleButtons.forEach(button => {
+    button.addEventListener('click', function() {
+        const showTimes = this.previousElementSibling;
+        if (showTimes.style.display === 'none') {
+            showTimes.style.display = 'block';
+            this.textContent = 'Ocultar Horarios';
+        } else {
+            showTimes.style.display = 'none';
+            this.textContent = 'Mostrar Horarios';
+        }
+    });
+});
